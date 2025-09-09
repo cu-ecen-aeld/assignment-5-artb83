2561609686d80d995dbfb79d4c44e5165afeb55c
artb83 <21041434+artb83@users.noreply.github.com>
2025-09-09 14:50:35 +0300

    Complete setup of base_external directory representing external build tree, for 1st build.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,7 +7,7 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = 958e98edc3aec2f698b9e80c114df7f7128a615f #last commit from assignment 3-2
-AESD_ASSIGNMENTS_VERSION = 2cc0a9794edaf4ef188ad83d0ed98e4f927c016a #first commit with assignment 4-2 br 1st time build.sh.
+AESD_ASSIGNMENTS_VERSION = 6dcc84d7fc5fabe6712f8590a0fcf842afbd2afd #first commit with assignment 4-2 br 1st time build.sh.
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -24,6 +24,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
